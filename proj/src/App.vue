91b864a2ce416982facd6b0dccb4e7e0edfba4b6
johnnyw <jwu38438@gmail.com>
2025-02-26 14:01:22 -0500

    e

--- proj/src/App.vue
+++ proj/src/App.vue
@@ -1,85 +1,68 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div v-for="item in arrestData" :key="item.arrest_key"
+  :style="
+  {
+    width:`500px`,
+    margin:`100px`
+  }">
+    {{ item.pd_desc }}
+    {{ item }}
+  </div>
 </template>
 
+
+
+<script setup>
+  import { onMounted, ref } from 'vue';
+  const arrestData =  ref('');
+  async function getData()
+  {
+    const fetchedData = await fetch("https://data.cityofnewyork.us/resource/8h9b-rp9u.json")
+    const jsonedData =  await fetchedData.json();
+    arrestData.value = jsonedData;
+  }
+  onMounted(()=>
+  {
+    getData();
+    console.log(arrestData)
+  })
+  console.log("testicle")
+  //data format
+  
+
+  const EXAMPLEDATA = {
+    "arrest_key": "279752532",
+    "arrest_date": "2023-12-31T00:00:00.000",
+    "pd_cd": "792",
+    "pd_desc": "CRIMINAL POSSESSION WEAPON",
+    "ky_cd": "118",
+    "ofns_desc": "DANGEROUS WEAPONS",
+    "law_code": "PL 2651B01",
+    "law_cat_cd": "F",
+    "arrest_boro": "K",
+    "arrest_precinct": "73",
+    "jurisdiction_code": "2",
+    "age_group": "25-44",
+    "perp_sex": "M",
+    "perp_race": "BLACK",
+    "x_coord_cd": "1009450",
+    "y_coord_cd": "182401",
+    "latitude": "40.66729",
+    "longitude": "-73.909161",
+    "lon_lat": {
+      "type": "Point",
+      "coordinates": [-73.909161, 40.66729]
+    },
+    ":@computed_region_efsh_h5xi": "17614",
+    ":@computed_region_f5dn_yrer": "55",
+    ":@computed_region_yeji_bk3q": "2",
+    ":@computed_region_92fq_4b7q": "17",
+    ":@computed_region_sbqj_enih": "46"
+  }
+
+
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
