9d86cb4c8bd9ab4a5c287e414f99e93fe22eca12
johnnyw <jwu38438@gmail.com>
2025-03-15 22:46:22 -0400

    egg

---
+++ proj/src/components/OptionsMenu.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="optionsBar">
+        <div class="options">Heat Map</div>
+        <div class="options">Chart Data</div>
+        <div class="options">Time Line</div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .options
+    {
+        width:200px;
+        height:10%;
+        overflow:hidden;
+        background-color: #f7e7ce;
+        display: flex;
+        justify-content: center;
+        transition: background-color 0.3s ease-in-out;
+    }
+    .optionsBar
+    {
+        text-align: center;
+        align-items: center;
+        width:200px;
+        height:100vh;
+        background-color:#f7e7ce;
+    }
+    .options:hover
+    {
+        background-color: #E3BC9A;
+    }
+    .changeStyling
+    {
+        background-color: black;
+        color:white;
+    }
+</style>
